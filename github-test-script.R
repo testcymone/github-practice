049939a4d49c64037ae1cdc2bd03b4c0a0978d48
Cymone Gates <38463305+cymonegates@users.noreply.github.com>
2021-10-06 12:42:16 -0700

    Create github-test-script.R

---
+++ github-test-script.R
@@ -0,0 +1,10 @@
+#################################################################################
+GitHub Test R Script
+#################################################################################
+
+# After creating a new branch off of the main branch, or after forking to your own GitHub account if you don't have write privileges to this repository,
+#   edit this program in any way you like.
+# After you edit, you'll need to commit the file with the new changes and then open a pull request;
+#      You can open a Pull Request at any point during the development process: when you have little or no code but want to share some screenshots or general ideas,
+#      when you're stuck and need help or advice, or when you're ready for someone to review your work.
+# By using GitHub's @mention system in your Pull Request message, you can ask for feedback from specific people or teams
